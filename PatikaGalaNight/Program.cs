583df89399cf7d1c1ce0cf0efbf5a4f004e5158c
Zeynep Tuana Çakır <2320711004@ogrenci.ibu.edu.tr>
2024-08-20 17:14:36 +0300

    Add project files.

---
+++ PatikaGalaNight/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<string> visitors = new List<string>();
+
+que: Console.WriteLine("Davetli girebilirsiniz: ");
+visitors.Add(Console.ReadLine());
+Console.WriteLine("Başka davetli eklemek ister misiniz? (e/h)");
+string c = Console.ReadLine();
+
+if (c == "e")
+    goto que;
+
+Console.WriteLine("\n----DAVETLİ LİSTESİ----\n");
+for (int i = 0; i < visitors.Count; i++)
+{
+    Console.WriteLine((i+1) + "- " +visitors[i]); // List indexim 0dan başladığı için +1 yazarak listeyi yazdırdım
+}
